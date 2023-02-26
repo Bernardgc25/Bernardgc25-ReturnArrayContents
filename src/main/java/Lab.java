79ec7726bb4f9e3d735e78f0d92678501e57f054
Bernardgc25 <Bernard_gcruz@yahoo.com>
2023-02-26 03:33:11 +0000

    completed: Return Array Contents

--- src/main/java/Lab.java
+++ src/main/java/Lab.java
@@ -30,6 +30,14 @@ public class Lab {
      * @return a string representation of all values of nums, with the values separated by spaces.
      */
     public String arrayToString(int[] nums){
-        return "";
+        StringBuilder temp = new StringBuilder();
+        String str;
+        for(int i = 0; i < nums.length; i++)
+        {
+        temp.append(nums[i]).append(" ");
+        }
+        str = temp.toString();
+        //return "";
+        return str;
     }
 }
